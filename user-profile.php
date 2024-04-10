60917af8069783ea00dcde22b376b488d0d17af7
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-04-10 23:55:25 +0100

    started profile page

---
+++ user-profile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FEUP-reUSE</title>
+    <link rel="icon" href="docs/images/REuse-mini.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+    <header>
+        <h1>
+            <a href="index.php">RE<strong>USE</strong></a>
+        </h1>
+    </header>
+    <section id="user-profile">
+        <div class="profile-info">
+            <img src="https://picsum.photos/200" alt="User Image">
+            <div class="profile-details">
+                <h2>User Name</h2>
+                <p>Email: user@gmail.com</p>
+            </div>
+        </div>
+        <div class="profile-content">
+            <div class="profile-actions">
+                <a href="#">User Details</a>
+                <a href="#">Wishlist</a>
+                <a href="#">Your Orders</a>
+            </div>
+            <!-- completar depois com ajax -->
+        </div>
+    </section>
+    <footer>
+        <p>&copy; FEUP-reUSE, 2024</p>
+    </footer>
+</body>
+</html>
