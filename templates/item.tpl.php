d05b92a1241b9719a0b52c1b8e615714d1744a4c
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-04-21 18:42:22 +0100

    cleaning up the code

--- templates/item.tpl.php
+++ templates/item.tpl.php
@@ -60,16 +60,79 @@
                 <p>Seller: <a href="../pages/user-profile.php?idUser=<?=$item->idSeller?>"><?=User::getUserById($db, $item->idSeller)->name?></a></p>
                 <p><span class="<?=$item->active ? 'active' : 'inactive'?>"><?=$item->active ? 'Active' : 'Inactive'?></span></p>
             </div>
-
-            <!-- Formulário de Mensagem -->
-            <div id="message-form">
-                <h3>Contact Seller</h3>
-                <form action="../utils/sendMessage.php" method="post">
-                    <input type="hidden" name="recipient" value="<?=$item->idSeller?>">
-                    <textarea name="message" rows="4" cols="50" placeholder="Enter your message here..."></textarea>
-                    <input type="submit" value="Send Message">
-                </form>
-            </div>
         </article>  
+        <!-- Formulário de Mensagem -->
+        <div id="message-form">
+            <h3>Contact Seller</h3>
+            <form action="../utils/sendMessage.php" method="post">
+                <input type="hidden" name="recipient" value="<?=$item->idSeller?>">
+                <textarea name="message" rows="4" cols="50" placeholder="Enter your message here..."></textarea>
+                <input type="submit" value="Send Message">
+            </form>
+        </div>
+    </section>
+<?php } ?>
+
+<?php function drawAddPublication(array $categories, array $sizes, array $conditions) { ?>
+    <?php
+        if (isset($_SESSION['message'])) {
+            echo "<p>{$_SESSION['message']}</p>";
+            unset($_SESSION['message']);
+        }
+    ?>
+    <section id = "add-publication">
+        <h1>Add New Publication</h1>
+        <form action="../actions/action_add_publication.php" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
+            <label>
+                Name: <input type="text" id="name" name="name" required>
+            </label><br>
+            <label>
+                Description: <input type="text" id="description" name="description" required>
+            </label><br>
+            <label>
+                Introduction: <input type="text" id="introduction" name="introduction" required>
+            </label><br>
+            <label>
+                Brand: <input type="text" id="brand" name="brand" required>
+            </label><br>
+            <label>
+                Model: <input type="text" id="model" name="model" required>
+            </label><br>
+            <label>
+                Price: <input type="number" id="price" name="price" required>
+            </label><br>
+            <label>
+                Category:
+                <select id="idCategory" name="idCategory" required>
+                    <?php foreach ($categories as $category) { ?>
+                        <option value="<?php echo $category->idCategory; ?>"><?php echo $category->categoryName; ?></option>
+                    <?php } ?>
+                </select>
+            </label><br>
+            <label>
+                Condition:
+                <select id="idCondition" name="idCondition" required>
+                    <?php foreach ($conditions as $condition) { ?>
+                        <option value="<?php echo $condition->idCondition; ?>"><?php echo $condition->conditionName; ?></option>
+                    <?php } ?>
+                </select>
+            </label><br>
+            <label>
+                Size:
+                <select id="idSize" name="idSize" required>
+                    <?php foreach ($sizes as $size) { ?>
+                        <option value="<?php echo $size->idSize; ?>"><?php echo $size->sizeName; ?></option>
+                    <?php } ?>
+                </select>
+            </label><br>
+            <label>
+                Upload Main Image: <input type="file" id="main_image" name="main_image" required>
+            </label><br>
+            <label>
+                Upload Secondary Images(max 5):<input type="file" id="secondary_images" name="secondary_images[]" multiple>
+                <span class="hint"> (Select multiple images from file browser)</span>
+            </label><br>
+            <button type="submit">Submit</button>
+        </form>
     </section>
 <?php } ?>
