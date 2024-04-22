0d365c9382860793703d2fb5f01752be83317984
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-04-22 22:22:22 +0100

    search working

--- pages/index.php
+++ pages/index.php
@@ -7,6 +7,8 @@
 
     require_once(__DIR__ . '/../database/connection.db.php');
     require_once(__DIR__ . '/../database/category.class.php');
+    require_once(__DIR__ . '/../database/size.class.php');
+    require_once(__DIR__ . '/../database/condition.class.php');
     require_once(__DIR__ . '/../database/item.class.php');
 
     require_once(__DIR__ . '/../templates/common.tpl.php');
@@ -15,11 +17,27 @@
     $db = getDatabaseConnection();
 
     $categories = Category::getCategories($db);
+    $sizes = Size::getSizes($db);
+    $conditions = Condition::getConditions($db);
     $items = Item::getItems($db, 10); //limitado a 10 items por enquanto
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        
+        $search = isset($_GET["search"]) ? $_GET["search"] : '';
+        $category = isset($_GET["category"]) ? $_GET["category"] : 'all';
+        $size = isset($_GET["size"]) ? $_GET["size"] : 'all';
+        $condition = isset($_GET["condition"]) ? $_GET["condition"] : 'all';
+        $order = isset($_GET["order"]) ? $_GET["order"] : 'default';
+
+        $items = Item::searchItems($db, $search, $category, $size, $condition, $order);
+    } 
+    else {
+        $items = Item::getItems($db, 10);
+    }
 
     drawHeader($session);
     drawCategories($categories);
-    drawSearchAndFilter();
+    drawSearchAndFilter($categories, $sizes, $conditions);
     drawItems($items, $db);
     drawFooter();
 ?>
